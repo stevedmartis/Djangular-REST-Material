3410349ff1bbd9543b702e59d1b783e0feeba6d8
demaro <demaromail@gmail.com>
2018-03-01 23:51:52 -0300

    update djangular v2

--- djang/wsgi.py
+++ djang/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from dj_static import Cling
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djang.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
